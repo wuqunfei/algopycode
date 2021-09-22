4524895ce7479f10c4ea7bf9ca7f02d28c75372c
Qunfei <wu.qunfei@gmail.com>
2021-09-22 17:12:08 +0200

    add files

---
+++ curve.py
@@ -0,0 +1,37 @@
+from datetime import date
+from typing import List, Dict
+
+
+class LeetCodeCurve:
+
+    def __init__(self):
+        self.review_days: List[int] = [1, 2, 4, 7, 15]
+        self.github_token = None
+
+    def find_question_by_commit(self) -> Dict:
+        """
+        1. question id key
+        2. review days
+        :return:{8:[], 146:[1,2,4]}
+        """
+        return {}
+
+    def review_question_by_day(self, day: date) -> List[int]:
+        """
+        :return:
+        """
+
+    def get_reports_by_status(self):
+        """
+
+        :return:
+        """
+
+    def get_report_by_tags(self):
+        """
+
+        :return:
+        """
+
+    def get_report_by_company(self, company_name: str):
+        """"""
