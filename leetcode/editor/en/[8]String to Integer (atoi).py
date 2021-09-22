e93299ef0417939a8fd80292003951e9f8226533
Qunfei <wu.qunfei@gmail.com>
2021-09-22 10:57:54 +0200

    clean status

--- leetcode/editor/en/[8]String to Integer (atoi).py
+++ leetcode/editor/en/[8]String to Integer (atoi).py
@@ -133,5 +133,6 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def myAtoi(self, s: str) -> int:
+        pass
         
 # leetcode submit region end(Prohibit modification and deletion)
