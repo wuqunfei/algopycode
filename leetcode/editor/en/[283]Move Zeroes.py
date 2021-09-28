63e071a92430b55288033742197a359fe5879bb5
Qunfei <wu.qunfei@gmail.com>
2021-09-28 10:18:44 +0200

    using swap in python

--- leetcode/editor/en/[283]Move Zeroes.py
+++ leetcode/editor/en/[283]Move Zeroes.py
@@ -36,9 +36,7 @@ class Solution:
         j = 0
         for i, node in enumerate(nums):
             if node != 0:
-                nums[j] = nums[i]
-                if i != j:
-                    nums[i] = 0
+                nums[j], nums[i] = nums[i], nums[j]
                 j += 1
         return nums
 
