68576944947e853c3c7d4daa7927fba85232e08c
Qunfei <wu.qunfei@gmail.com>
2021-09-28 15:50:23 +0200

    git add has cycle issue

--- leetcode/editor/en/[15]3Sum.py
+++ leetcode/editor/en/[15]3Sum.py
@@ -27,13 +27,27 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 from typing import List
 
+"""
+ Not O
+"""
+
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        rev = []
+        res, dups = set(), set()
+        seen = {}
+        for i, val1 in enumerate(nums):
+            if val1 not in dups:
+                dups.add(val1)
+                for j, val2 in enumerate(nums[i+1:]):
+                    complement = -val1 - val2
+                    if complement in seen and seen[complement] == i:
+                        res.add(tuple(sorted((val1, val2, complement))))
+                    seen[val2] = i
+        return list(res)
 
 
-        return rev
-
-        
 # leetcode submit region end(Prohibit modification and deletion)
+s = Solution()
+x = s.threeSum([-1, 0, 1, 2, -1, -4])
+print(x)
