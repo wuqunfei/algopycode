a1e44f9207eb5492b744b3df012ac4b58a6557bc
Qunfei <wu.qunfei@gmail.com>
2021-09-29 16:47:07 +0200

    sliding windows

--- leetcode/editor/en/[239]Sliding Window Maximum.py
+++ leetcode/editor/en/[239]Sliding Window Maximum.py
@@ -66,6 +66,10 @@
 from typing import List
 from collections import deque
 
+"""
+Time O(N)
+Space O(3)
+"""
 
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
