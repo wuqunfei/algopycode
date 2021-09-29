e445af334e3bb4e08b5f3dd4830910c53bafee01
Qunfei <wu.qunfei@gmail.com>
2021-09-29 13:47:55 +0200

    add stack question

---
+++ leetcode/editor/en/[20]Valid Parentheses.py
@@ -0,0 +1,75 @@
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']
+# ', determine if the input string is valid. 
+# 
+#  An input string is valid if: 
+# 
+#  
+#  Open brackets must be closed by the same type of brackets. 
+#  Open brackets must be closed in the correct order. 
+#  
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: s = "()"
+# Output: true
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: s = "()[]{}"
+# Output: true
+#  
+# 
+#  Example 3: 
+# 
+#  
+# Input: s = "(]"
+# Output: false
+#  
+# 
+#  Example 4: 
+# 
+#  
+# Input: s = "([)]"
+# Output: false
+#  
+# 
+#  Example 5: 
+# 
+#  
+# Input: s = "{[]}"
+# Output: true
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= s.length <= 10⁴ 
+#  s consists of parentheses only '()[]{}'. 
+#  
+#  Related Topics String Stack 👍 9175 👎 363
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def isValid(self, s: str) -> bool:
+        parentheses = {'(': ')', '{': '}', '[': ']', '#': '#'}
+        stack = []
+        for char in s:
+            if char in parentheses:
+                stack.append(char)
+            else:
+                top = stack.pop() if stack else '#'
+                if parentheses[top] != char:
+                    return False
+        return not stack
+
+
+# leetcode submit region end(Prohibit modification and deletion)
+
+s = Solution()
+print(s.isValid("()[]{}"))
