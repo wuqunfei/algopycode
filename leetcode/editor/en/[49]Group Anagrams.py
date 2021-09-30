2a3f2f4a3ef2753113dfcc8e96b9a821e160d0bb
Qunfei <wu.qunfei@gmail.com>
2021-09-30 12:00:58 +0200

    tuple as key and defaultlist and dict

--- leetcode/editor/en/[49]Group Anagrams.py
+++ leetcode/editor/en/[49]Group Anagrams.py
@@ -34,14 +34,11 @@ from typing import List
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        ans = collections.defaultdict(list)
-        for s in strs:
-            ans[tuple(sorted(s))].append(s)
-        return ans.values()
+        rev = collections.defaultdict(list)
+        for str in strs:
+            key = tuple(sorted(str))
+            rev[key].append(str)
+        return rev.values()
 
 
 # leetcode submit region end(Prohibit modification and deletion)
-
-s = Solution()
-x = s.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])
-print(x)
