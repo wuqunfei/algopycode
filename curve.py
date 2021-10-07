95748e2e6d269e7d12d792e7837249d9e6559b24
Qunfei <wu.qunfei@gmail.com>
2021-10-07 15:39:13 +0200

    add quesition commmits

--- curve.py
+++ curve.py
@@ -1,3 +1,4 @@
+import glob
 from datetime import date
 from typing import List, Dict
 import git
@@ -11,6 +12,7 @@ class LeetCodeCurve:
         self.github_repository = 'https://github.com/wuqunfei/algopycode.git'
         self.repository_path = '.'
         self.repository = git.Repo(self.repository_path)
+        self.path = './leetcode/editor/en/*.py'
 
     def __int__(self):
         ...
@@ -23,10 +25,27 @@ class LeetCodeCurve:
         :return:{8:[], 146:[1,2,4]}
         """
         tree = self.repository.tree()
-        for blob in tree.blobs:
-            commits = list(self.repository.iter_commits(paths=blob.path, max_count=-1))
-            print(commits)
-        return {}
+        questions = []
+        for file_path in glob.glob(self.path):
+            commits = list(self.repository.iter_commits(paths=file_path, max_count=-1))
+            question = self.get_question_info(file_path, commits)
+            if question:
+                questions.append(question)
+        return questions
+
+    def get_question_info(self, file_path, commits):
+        left_index = file_path.find('[')
+        right_index = file_path.find(']') + 1
+        if left_index and right_index:
+            question_id = file_path[left_index: right_index]
+            question_name = file_path[right_index:]
+            question_dates = []
+            for commit in commits:
+                question_dates.append(commit.committed_datetime)
+
+            return {'question_id': question_id,
+                    'question_name': question_name,
+                    'question_dates': question_dates}
 
     def review_question_by_day(self, day: date) -> List[int]:
         """
