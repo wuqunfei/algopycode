382e914cfb4892960e14972cedc1dd57d5ca7bdf
Qunfei <wu.qunfei@gmail.com>
2021-10-07 17:07:44 +0200

    adding commits

--- curve.py
+++ curve.py
@@ -1,5 +1,6 @@
 import glob
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
+from ordered_set import OrderedSet
 from pytz import timezone
 from typing import List
 import git
@@ -11,20 +12,19 @@ class Question:
         self.question_id: str = None
         self.question_name: str = None
         self.dates: List[date] = None
-        self.reviews: set = set()
-
+        self.reviewed: OrderedSet = OrderedSet()
+        self.reviews: dict = {1: {}, 2: {}, 4: {}, 7: {}, 15: {}}
 
     def __str__(self):
         return f'id: {self.question_id}, ' \
                f'name: {self.question_name}, ' \
                f'reviews: {self.reviews}, ' \
-               f'dates: {self.dates}'
+               f'reviewed: {self.reviewed}'
 
 
 class LeetCodeCurve:
 
     def __init__(self):
-        self.review_days: List[int] = [1, 2, 4, 7, 15]
         self.github_token = None
         self.github_repository = 'https://github.com/wuqunfei/algopycode.git'
         self.repository_path = '.'
@@ -71,12 +71,19 @@ class LeetCodeCurve:
             day = datetime.now()
         tz = timezone('Europe/Berlin')
         for index, question in enumerate(questions):
-            for committed_day in question.dates:
+            for j, committed_day in enumerate(sorted(question.dates)):
+                if j == 0:
+                    questions[index] = self.review_match(question, committed_day)
                 day_delta = tz.localize(day) - committed_day
                 days = day_delta.days
-                question.reviews.add(days)
+                question.reviewed.add(days)
             questions[index] = question
         return questions
+
+    def review_match(self, question: Question, start_day: date):
+        for key, value in question.reviews.items():
+            question.reviews[key] = start_day + timedelta(days=key)
+        return question
 
 
 curve = LeetCodeCurve()
