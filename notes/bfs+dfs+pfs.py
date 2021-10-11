da0a134cb2edabda012b0db0d3e9131b5d749fed
Qunfei <wu.qunfei@gmail.com>
2021-10-11 09:54:37 +0200

    BFS, DFS, PFS tempalte in python

---
+++ notes/bfs+dfs+pfs.py
@@ -0,0 +1,40 @@
+import collections
+from queue import PriorityQueue
+
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left: Node = None
+        self.right: Node = None
+
+
+class Search:
+
+    def __init__(self):
+        self.visited = set()
+        self.queue = collections.deque()
+
+    def depth_first_search(self, node: Node):
+        if node in self.visited:
+            return
+        self.visited.add(node)
+        # find the children node and go next
+        for leaf in self.children():
+            if leaf not in self.visited:
+                self.depth_first_search(node)
+
+    def breath_first_search(self, node: Node):
+        self.queue.append(node)
+        while self.queue:
+            leaf = self.queue.popleft()
+            self.visited.add(leaf)
+            nodes = self.children(node)
+            self.queue.append(nodes)
+
+    def priority_search(self, node: PriorityQueue):
+        while node:
+            node.get(block=False, timeout=2)
+
+    def children(self, node: Node):
+        return [node.left, node.right]
