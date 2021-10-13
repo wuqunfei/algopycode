0b7cd42ceeba324193aebb09a506a55b563dfaed
Qunfei <wu.qunfei@gmail.com>
2021-10-13 14:59:53 +0200

    add rotated sorted array 2

--- leetcode/editor/en/[81]Search in Rotated Sorted Array II.py
+++ leetcode/editor/en/[81]Search in Rotated Sorted Array II.py
@@ -38,7 +38,32 @@
 
 
 # leetcode submit region begin(Prohibit modification and deletion)
+from typing import List
+
+
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
-        
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return True
+            if nums[left] == nums[mid]:
+                left += 1
+            elif nums[mid] <= nums[right]:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            else:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+        return False
+
+
 # leetcode submit region end(Prohibit modification and deletion)
+
+solution = Solution()
+solution.search([2, 5, 6, 0, 0, 1, 2], 1)
