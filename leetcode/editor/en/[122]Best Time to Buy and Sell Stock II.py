3be5f42c6a000ccad98ed798b6cb8e24df19a08e
Qunfei <wu.qunfei@gmail.com>
2021-10-16 11:28:39 +0200

    add dp in the code

--- leetcode/editor/en/[122]Best Time to Buy and Sell Stock II.py
+++ leetcode/editor/en/[122]Best Time to Buy and Sell Stock II.py
@@ -62,8 +62,22 @@ class Solution:
                 profit += prices[i + 1] - prices[i]
         return profit
 
+    # https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/solution/tan-xin-suan-fa-by-liweiwei1419-2/
+    def max_dp(self, prices: List[int]) -> int:
+        days = len(prices)
+        dp = [[0] * 2 for _ in range(days)]
+        # 0: having cash
+        # 1: having stock
+        dp[0][0] = 0
+        dp[0][1] = - prices[0]
+        for i in range(1, days, 1):
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i])
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i])
+        return dp[days - 1][0]
+
 
 # leetcode submit region end(Prohibit modification and deletion)
 
 solution = Solution()
 print(solution.maxProfit([7, 1, 5, 3, 6, 4]))
+print(solution.max_dp([7, 1, 5, 3, 6, 4]))
