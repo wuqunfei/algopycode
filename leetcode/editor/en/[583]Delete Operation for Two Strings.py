d0e044d8279cc13b4abb8585d7e6d6c76b3709f4
Qunfei <wu.qunfei@gmail.com>
2021-10-19 14:44:10 +0200

    add min distance

---
+++ leetcode/editor/en/[583]Delete Operation for Two Strings.py
@@ -0,0 +1,51 @@
+# Given two strings word1 and word2, return the minimum number of steps 
+# required to make word1 and word2 the same. 
+# 
+#  In one step, you can delete exactly one character in either string. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: word1 = "sea", word2 = "eat"
+# Output: 2
+# Explanation: You need one step to make "sea" to "ea" and another step to make 
+# "eat" to "ea".
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: word1 = "leetcode", word2 = "etco"
+# Output: 4
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= word1.length, word2.length <= 500 
+#  word1 and word2 consist of only lowercase English letters. 
+#  
+#  Related Topics String Dynamic Programming 👍 2217 👎 39
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        m = len(word1)
+        n = len(word2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if word1[i - 1] == word2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        return m + n - dp[m][n] * 2
+
+    # leetcode submit region end(Prohibit modification and deletion)
+
+
+solution = Solution()
+print(solution.minDistance('park', 'spake'))
